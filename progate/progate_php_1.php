ba899d5c5acc38438088f3a933039917c024cab0
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-08-23 23:54:48 +0900

    done 18

--- progate/progate_php_1.php
+++ progate/progate_php_1.php
@@ -192,3 +192,25 @@ echo '<br>';
 echo rand(10, 15);
 
 ?>
+<?php
+
+// 関数helloを定義してください
+function hello()
+{
+  echo "Hello, world!";
+}
+
+// 関数helloを呼び出してください
+hello();
+
+echo '<br>';
+// 関数printRectangleAreaを定義してください
+function printRectangleArea($height, $width)
+{
+  echo $height * $width;
+}
+
+// 引数を(5, 10)としてprintRectangleAreaを呼び出してください
+printRectangleArea(5, 10);
+
+?>
