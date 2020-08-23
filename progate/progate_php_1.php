f5ba93bdd0325f5c3f7ff6eb68c8e7db52c83cad
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-08-23 23:51:04 +0900

    done 17

--- progate/progate_php_1.php
+++ progate/progate_php_1.php
@@ -171,3 +171,24 @@ foreach ($scores as $key => $value) {
 }
 
 ?>
+
+<?php
+
+$str = 'progate';
+
+// strlenを用いて$strの長さをechoしてください
+echo strlen($str);
+
+echo '<br>';
+
+$array = array('HTML', 'CSS', 'PHP');
+
+// countを用いて$arrayの要素数をechoしてください
+echo count($array);
+
+echo '<br>';
+
+// randを用いて10から15までのランダムな数字をechoしてください
+echo rand(10, 15);
+
+?>
