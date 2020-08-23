ca8c2e4e9e51ab0be47b2baa515a2f3fdfc3f8bb
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-08-23 23:47:01 +0900

    done 16

--- progate/progate_php_1.php
+++ progate/progate_php_1.php
@@ -161,3 +161,13 @@ for ($i = 1; $i < 1001; $i++) {
 }
 
 ?>
+<?php
+
+$scores = array('数学' => 70, '英語' => 90, '国語' => 80);
+
+// この下にforeach文を書いてください
+foreach ($scores as $key => $value) {
+  echo "{$key}は{$value}点です。";
+}
+
+?>
