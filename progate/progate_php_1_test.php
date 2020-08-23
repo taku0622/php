205fecca28d268ec37ef769bebaa1f7443cb9a01
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-08-24 03:26:03 +0900

    done test2

--- progate/progate_php_1_test.php
+++ progate/progate_php_1_test.php
@@ -2,3 +2,15 @@
 echo "Hello, PHP";
 echo "<br>";
 echo "10 + 7";
+?>
+<?php
+$name = 'Tom';
+echo '変数$nameの値: ' . $name;
+echo '<br>';
+echo '-----';
+echo '<br>';
+
+// この下にコードを書いてください
+echo "My name is {$name}"
+
+?>
