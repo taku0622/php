ae606b7dbdce5f3570ebe3ec118079317a947929
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-08-23 23:57:32 +0900

    done 19

--- progate/progate_php_1.php
+++ progate/progate_php_1.php
@@ -214,3 +214,19 @@ function printRectangleArea($height, $width)
 printRectangleArea(5, 10);
 
 ?>
+
+<?php
+
+// 関数getCircleAreaを定義してください
+function getCircleArea($radius)
+{
+  return $radius * $radius * 3;
+}
+
+// 関数getCircleAreaを呼び出して、戻り値を変数$circleAreaに代入してください
+$circleArea = getCircleArea(5);
+
+// $circleAreaをechoしてください
+echo $circleArea;
+
+?>
