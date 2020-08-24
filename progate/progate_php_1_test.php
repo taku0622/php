be2bd23a6ac15bffb9fa8b8d1b02106ff2cc103c
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-08-24 15:11:02 +0900

    done test10

--- progate/progate_php_1_test.php
+++ progate/progate_php_1_test.php
@@ -136,3 +136,22 @@ foreach ($menus as $menu) {
 }
 
 ?>
+
+<?php
+$menus = array(
+  array('name' => 'CURRY', 'price' => 900),
+  array('name' => 'PASTA', 'price' => 1200),
+  array('name' => 'COFFEE', 'price' => 600)
+);
+
+$totalPrice = 0; // 合計金額を入れるための変数
+foreach ($menus as $menu) {
+  $price = $menu['price'];
+  echo $menu['name'] . 'は' . $price . '円です';
+  echo '<br>';
+  // それぞれの価格を$totalPriceに足していく
+  $totalPrice += $price;
+}
+echo '合計金額は' . $totalPrice . '円です';
+
+?>
