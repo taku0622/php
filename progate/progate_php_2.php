b7f89f77c68d4ef865e9196d7616746583ffc9be
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-08-24 16:07:00 +0900

    done PHP_2_4

--- progate/progate_php_2.php
+++ progate/progate_php_2.php
@@ -57,3 +57,24 @@ echo '<br>';
 $pasta->hello();
 
 ?>
+
+<?php
+class Menu {
+  public $name;
+  
+  public function hello() {
+    // '私は○○です'とechoしてください
+    echo "私は{$this->name}です";
+    
+  }
+}
+
+$curry = new Menu();
+$pasta = new Menu();
+$curry->name = 'CURRY';
+$pasta->name = 'PASTA';
+$curry->hello();
+echo '<br>';
+$pasta->hello();
+
+?>
