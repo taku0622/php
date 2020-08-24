c61b62b201b7a3ba9da79e2e7d804371f596b764
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-08-24 14:43:34 +0900

    done test8

--- progate/progate_php_1_test.php
+++ progate/progate_php_1_test.php
@@ -107,3 +107,17 @@ echo "合計金額は{$sum}円です";
 echo '<br>';
 echo "最高価格は{$max}円です";
 ?>
+
+<?php
+$menu = array('name' => 'CURRY', 'price' => 900);
+echo '$menuの値: ';
+// var_exportは変数の中身を見るための関数です
+var_export($menu);
+echo '<br>';
+echo '-----';
+echo '<br>';
+
+// この下にコードを書いてください
+echo "{$menu['name']}は{$menu['price']}円です"
+
+?>
