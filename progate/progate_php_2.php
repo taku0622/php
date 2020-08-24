fb850b6ea04fde5af03d187693ddad7ce6944ed3
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-08-24 16:00:44 +0900

    done PHP_2_2

--- progate/progate_php_2.php
+++ progate/progate_php_2.php
@@ -9,3 +9,27 @@ $curry = new Menu();
 
 // Menuクラスのインスタンスを作り、変数$pastaに代入してください
 $pasta = new Menu();
+
+<?php
+class Menu {
+  // $nameというプロパティを定義してください
+  public $name;
+  
+}
+
+$curry = new Menu();
+$pasta = new Menu();
+// $curryのnameプロパティに'CURRY'を代入してください
+$curry->name = "CURRY";
+
+// $pastaのnameプロパティに'PASTA'を代入してください
+$pasta->name = "PASTA";
+
+// $curryのnameプロパティをechoしてください
+echo $curry->name;
+
+echo '<br>';
+// $pastaのnameプロパティをechoしてください
+echo $pasta->name;
+
+?>
