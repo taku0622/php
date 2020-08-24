a8f8ee6abe940b6a7e3c0510d3a8fb5bfbf135fd
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-08-24 16:26:21 +0900

    done PHP_2_7

--- progate/progate_php_2.php
+++ progate/progate_php_2.php
@@ -128,3 +128,27 @@ echo '<br>';
 $pasta->hello();
 
 ?>
+
+<?php
+class Menu {
+  public $name;
+  
+  public function __construct($name) {
+    $this->name = $name;
+  }
+  
+  public function hello() {
+    echo '私は'.$this->name.'です';
+  }
+}
+
+$curry = new Menu('CURRY');
+$pasta = new Menu('PASTA');
+
+?>
+
+<!-- <p>タグの中で、$curryのnameプロパティを表示してください -->
+<p><?php echo $curry->name ?></p>
+
+<!-- <p>タグの中で、$pastaのnameプロパティを表示してください -->
+<p><?php echo $pasta->name ?></p>
