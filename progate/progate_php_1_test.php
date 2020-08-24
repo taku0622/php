cc00eeb736a9d74b8cfa735c56b65024584740d8
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-08-24 14:29:46 +0900

    done test4

--- progate/progate_php_1_test.php
+++ progate/progate_php_1_test.php
@@ -29,3 +29,27 @@ $result = $price + $price * $taxRate;
 echo "税込価格は{$result}円です";
 
 ?>
+
+<?php
+$money = 2000;
+$price = 1000;
+$taxRate = 0.08;
+echo '変数$moneyの値: ' . $money;
+echo '<br>';
+echo '変数$priceの値: ' . $price;
+echo '<br>';
+echo '変数$taxRateの値: ' . $taxRate;
+echo '<br>';
+echo '-----';
+echo '<br>';
+
+// この下にコードを書いてください
+$result = $price + $price * $taxRate;
+if ($money > $result) {
+  echo "商品を買うことができます";
+} elseif ($money == $result) {
+  echo "商品を買うことができますが、所持金がなくなります";
+} else {
+  echo "商品を買うことができません";
+}
+?>
