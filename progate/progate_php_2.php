c170cc3df3ac9125c981d7bddad1085eda176535
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-08-24 16:13:29 +0900

    done PHP_2_6

--- progate/progate_php_2.php
+++ progate/progate_php_2.php
@@ -103,3 +103,28 @@ echo '<br>';
 $pasta->hello();
 
 ?>
+<?php
+class Menu {
+  public $name;
+  
+  // コンストラクタの引数に$nameを入れてください
+  public function __construct($name) {
+    // nameプロパティに引数の$nameを代入してください
+    $this->name = $name;
+    
+  }
+  
+  public function hello() {
+    echo '私は'.$this->name.'です';
+  }
+}
+
+// new Menu()の引数に'CURRY'を入れてください
+$curry = new Menu("CURRY");
+// new Menu()の引数に'PASTA'を入れてください
+$pasta = new Menu("PASTA");
+$curry->hello();
+echo '<br>';
+$pasta->hello();
+
+?>
