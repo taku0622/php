abdfb4fcd8255fa55fe9eaf74bc87d8c9d2f4e65
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-08-26 02:10:14 +0900

    done php_4_8

--- progate/php4/menu.php
+++ progate/php4/menu.php
@@ -50,12 +50,24 @@ class Menu
     return $this->getTaxIncludedPrice() * $this->orderCount;
   }
 
+  // getReviewsメソッドを定義してください
+  public function getReviews($reviews)
+  {
+    $reviewsForMenu = array();
+    foreach ($reviews as $review) {
+      if ($review->getMenuName() == $this->name) {
+        $reviewsForMenu[] = $review;
+      }
+    }
+    return $reviewsForMenu;
+  }
+
+
   public static function getCount()
   {
     return self::$count;
   }
 
-  // findByNameというクラスメソッドを定義してください
   public static function findByName($menus, $name)
   {
     foreach ($menus as $menu) {
