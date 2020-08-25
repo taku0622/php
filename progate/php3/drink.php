a913938091141a1fac3c55618cb4b5d6829843ac
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-08-25 16:44:57 +0900

    done php_3_4

---
+++ progate/php3/drink.php
@@ -0,0 +1,8 @@
+<?php
+// menu.phpを読み込んでください
+require_once('menu.php');
+
+// Menuクラスを継承したDrinkクラスを作成してください
+class Drink extends Menu
+{
+}
---
+++ progate/php3/food.php
@@ -0,0 +1,8 @@
+<?php
+// menu.phpを読み込んでください
+require_once('menu.php');
+
+// Menuクラスを継承したFoodクラスを作成してください
+class Food extends Menu
+{
+}
