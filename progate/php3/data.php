b15cf8fa3305fdc6dc73f63d400bc927a6f49f51
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-08-25 17:21:02 +0900

    done php_3_9

--- progate/php3/data.php
+++ progate/php3/data.php
@@ -2,12 +2,11 @@
 require_once('drink.php');
 require_once('food.php');
 
-// $juiceのnewの引数に'アイス'を追加してください
-$juice = new Drink('JUICE', 600, 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/juice.png', "アイス");
-// $coffeeのnewの引数に'ホット'を追加してください
-$coffee = new Drink('COFFEE', 500, 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/coffee.png', "ホット");
-$curry = new Food('CURRY', 900, 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/curry.png');
-$pasta = new Food('PASTA', 1200, 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/pasta.png');
-
+$juice = new Drink('JUICE', 600, 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/juice.png', 'アイス');
+$coffee = new Drink('COFFEE', 500, 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/coffee.png', 'ホット');
+// $curryの引数に数値の3を追加してください
+$curry = new Food('CURRY', 900, 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/curry.png', 3);
+// $pastaの引数に数値の1を追加してください
+$pasta = new Food('PASTA', 1200, 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/pasta.png', 1);
 
 $menus = array($juice, $coffee, $curry, $pasta);
--- progate/php3/food.php
+++ progate/php3/food.php
@@ -1,8 +1,21 @@
 <?php
-// menu.phpを読み込んでください
 require_once('menu.php');
 
-// Menuクラスを継承したFoodクラスを作成してください
 class Food extends Menu
 {
+  // $spicinessというprivateなプロパティを定義してください
+  private $spiciness;
+
+  // 親クラスの__constructをオーバーライドしてください
+  public function __construct($name, $price, $image, $spiciness)
+  {
+    parent::__construct($name, $price, $image);
+    $this->spiciness = $spiciness;
+  }
+
+  // getSpicinessメソッドを定義してください
+  public function getSpiciness()
+  {
+    return $this->spiciness;
+  }
 }
