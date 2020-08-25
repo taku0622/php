e1ab5fa45578a5e700db42c7f89b6dbbfb84da9a
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-08-26 02:32:22 +0900

    done php_4_11

--- progate/php4/review.php
+++ progate/php4/review.php
@@ -1,13 +1,19 @@
 <?php
-// Reviewクラスを定義してください
 class Review
 {
   private $menuName;
+  // $userNameというprivateなプロパティを定義してください
+  private $userName;
+
   private $body;
 
-  public function __construct($menuName, $body)
+  // 2つ目の引数として$userNameを追加してください
+  public function __construct($menuName, $userName, $body)
   {
     $this->menuName = $menuName;
+    // userNameプロパティに$userNameを代入してください
+    $this->userName = $userName;
+
     $this->body = $body;
   }
 
@@ -19,5 +25,15 @@ class Review
   public function getBody()
   {
     return $this->body;
+  }
+
+  // getUserメソッドを定義してください
+  public function getUser($users)
+  {
+    foreach ($users as $user) {
+      if ($user->getName() == $this->userName) {
+        return $user;
+      }
+    }
   }
 }
