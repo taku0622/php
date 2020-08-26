034bc0a2c67f8b728f109c431f2f8be6b6609840
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-08-26 20:37:40 +0900

    done 176_A

---
+++ AtCoder/ABC176_A.php
@@ -0,0 +1,11 @@
+<?php
+# 整数の入力
+// fscanf(STDIN, "%d", $a);
+# スペース区切りの整数の入力
+fscanf(STDIN, "%d %d %d", $n, $x, $t);
+# 文字列の入力
+for ($i = 0; $n > $x * $i; $i++) {
+}
+// fscanf(STDIN, "%s", $s);
+# 出力
+echo $i * $t . "\n";
