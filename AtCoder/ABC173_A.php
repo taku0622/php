2b58085799a799a4d2a5592e625fc20e8bdfecec
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-08-26 21:02:03 +0900

    done 173_A

---
+++ AtCoder/ABC173_A.php
@@ -0,0 +1,29 @@
+<?php
+# 入力部
+// fscanf(STDIN, "%d", $a);
+// fscanf(STDIN, "%d %d %d", $n, $x, $t);
+// fscanf(STDIN, "%s", $s);
+fscanf(STDIN, "%d", $n);
+
+# 処理部
+// for ($i = 0; $n > $x * $i; $i++) {
+// }
+// if (strpos($s, 'RRR') !== false) {
+//   $r = 3;
+// } elseif (strpos($s, 'RR') !== false) {
+//   $r = 2;
+// } elseif (strpos($s, 'R') !== false) {
+//   $r = 1;
+// } else {
+//   $r = 0;
+// }
+// $s = "No";
+// if ($x >= 30) {
+//   $s = "Yes";
+// }
+for ($i = 0; $i * 1000 < $n; $i++) {
+}
+
+# 出力部
+// echo $r . "\n";
+echo ($i * 1000) - $n . "\n";
