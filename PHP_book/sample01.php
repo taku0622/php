869e83712543f6cebe9c573c11dbddda856818b0
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-08-29 01:57:11 +0900

    done ch3-4

--- PHP_book/sample01.php
+++ PHP_book/sample01.php
@@ -3,3 +3,5 @@ print("PHPを勉強中です!\n");
 print('takumi');
 print("現在は" . date("G-i-s") . "\n");
 print("今日は" . date("Y年 n月 j日") . "です\n");
+$today = new DateTime();
+print("現在は" . $today->format("G時 i分 s秒") . "\n");
