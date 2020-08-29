dcc0e57d3aeff3d4ef47fc4dddfba53206dc88fb
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-08-29 13:06:14 +0900

    done ch3-8

--- PHP_book/sample01.php
+++ PHP_book/sample01.php
@@ -27,14 +27,18 @@ $sum = 100 + 1050 + 200;
 //   print($i . "\n");
 //   $i -= 2;
 // }
-for ($i = 1; $i <= 365; $i++) {
-  $day = date('n/j(D)', strtotime('+' . $i . 'day'));
-  print($day . "\n");
-}
-$i = 1;
-while ($i <= 365) {
-  $timestamp = strtotime("+" . $i . "day");
-  $day = date("n/j(D)", $timestamp);
-  print($day . "\n");
-  $i++;
-}
+// for ($i = 1; $i <= 365; $i++) {
+//   $day = date('n/j(D)', strtotime('+' . $i . 'day'));
+//   print($day . "\n");
+// }
+// $i = 1;
+// while ($i <= 365) {
+//   $timestamp = strtotime("+" . $i . "day");
+//   $day = date("n/j(D)", $timestamp);
+//   print($day . "\n");
+//   $i++;
+// }
+$week_name = ["日", "月", "火", "水", "木", "金", "土"];
+print("今日は" . $week_name[date("w")] . "曜日です\n");
+$ages = ["10代以下", "20代", "30代", "40代", "50代", "60台以上"];
+print($ages[1] . "\n");
