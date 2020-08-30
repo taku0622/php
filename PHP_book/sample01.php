f3623608790a636e136bff8b46e306139a6be823
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-08-30 13:35:48 +0900

    done ch3-19

--- PHP_book/sample01.php
+++ PHP_book/sample01.php
@@ -118,3 +118,11 @@ foreach ($json->items as $item) :
   <p>性別：<input type="radio" name="gender" value="male"> 男性 ／ <input type="radio" name="gender" value="female"> 女性</p>
   <input type="submit" value="送信する">
 </form>
+<p>ご予約希望日（複数選択可）</p>
+
+<p>
+  <input type="checkbox" name="reserve[]" value="1/1"> 1月 1日<br>
+  <input type="checkbox" name="reserve[]" value="1/2"> 1月 2日<br>
+  <input type="checkbox" name="reserve[]" value="1/3"> 1月 3日<br>
+</p>
+<input type="submit" value="送信する">
