f5f27ef4113893aa7f995fc01f24e6f2c2232598
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-09-03 13:01:20 +0900

    done 1-3

--- progate/sql1/exercise.sql
+++ progate/sql1/exercise.sql
@@ -7,3 +7,13 @@ FROM purchases;
 
 SELECT price
 FROM purchases;
+
+-- purchasesテーブルから、「nameカラム」と「priceカラム」のデータを取得してください
+
+SELECT name, price
+FROM purchases;
+
+-- purchasesテーブルから、全てのカラムのデータを取得してください
+
+SELECT *
+FROM purchases;
