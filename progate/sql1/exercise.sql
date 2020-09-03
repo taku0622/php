ca10bb7cd442381e8fcc8176a65da5f984b78d36
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-09-03 14:00:12 +0900

    done 1-11

--- progate/sql1/exercise.sql
+++ progate/sql1/exercise.sql
@@ -91,6 +91,19 @@ SELECT *
 FROM purchases
 WHERE price IS NOT NULL;
 
+-- 「FROM purchases」のあとにコードを追加し、categoryカラムが「食費」かつcharacter_nameカラムが「ひつじ仙人」であるデータを取得してください
+
+SELECT *
+FROM purchases
+WHERE category ="食費"
+  AND character_name = "ひつじ仙人";
+
+-- 「FROM purchases」のあとにコードを追加し、categoryカラムが「食費」またはcharacter_nameカラムが「にんじゃわんこ」であるデータを取得してください
+
+SELECT *
+FROM purchases
+WHERE category = "食費"
+  OR character_name = "にんじゃわんこ";
 
 
 
