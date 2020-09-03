113a5e296e23b1fb68ddb3ea230dc6a3de54695c
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-09-04 05:03:21 +0900

    done 3-11

--- progate/sql3/exercise.sql
+++ progate/sql3/exercise.sql
@@ -89,3 +89,23 @@ FROM players
   ON players.country_id = countries.id
   LEFT JOIN teams
   ON players.previous_team_id = teams.id;
+
+SELECT name AS "選手名", height AS "身長"
+FROM players
+WHERE height > (
+  SELECT AVG(height)
+FROM players
+);
+
+SELECT *
+FROM players
+  JOIN countries
+  ON players.country_id = countries.id
+WHERE countries.name ="日本"
+  AND players.height >= 180;
+
+SELECT countries.name AS "国名", AVG(players.goals) AS "平均得点"
+FROM players
+  JOIN countries
+  ON players.country_id = countries.id
+GROUP BY countries.name;
