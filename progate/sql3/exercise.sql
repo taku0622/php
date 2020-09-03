9b0b6e49cca75ed7d7c8a8988092a83dd0817169
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-09-04 00:46:44 +0900

    done 3-2

--- progate/sql3/exercise.sql
+++ progate/sql3/exercise.sql
@@ -5,3 +5,20 @@ WHERE name ="ウィル";
 SELECT *
 FROM players
 WHERE goals > 14;
+
+SELECT name
+FROM players
+WHERE goals > (
+  -- この下にウィルの得点数を取得するクエリを書いてください
+  SELECT goals
+FROM players
+WHERE name ="ウィル"
+)
+;
+
+SELECT name, goals
+FROM players
+WHERE goals > (
+  SELECT AVG(goals)
+FROM players
+);
