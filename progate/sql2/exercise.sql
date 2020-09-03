2f89664b0e87dd39438d2201513428571d5c96bf
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-09-03 23:11:27 +0900

    done 2-9

--- progate/sql2/exercise.sql
+++ progate/sql2/exercise.sql
@@ -92,3 +92,20 @@ GROUP BY purchased_at, character_name;
 SELECT COUNT(*), purchased_at, character_name
 FROM purchases
 GROUP BY purchased_at,character_name;
+
+-- WHEREにcharacter_nameがにんじゃわんこであるデータを指定し、
+-- グループ化してください
+
+SELECT SUM(price), purchased_at
+FROM purchases
+WHERE character_name = "にんじゃわんこ"
+GROUP BY purchased_at;
+
+-- WHEREに条件を付け足してcategoryが食費であるデータを
+-- purchased_atとcharacter_nameでグループ化してください
+
+SELECT SUM(price), purchased_at, character_name
+FROM purchases
+WHERE category = "食費"
+GROUP BY purchased_at,character_name
+;
