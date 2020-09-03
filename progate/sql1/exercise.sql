d12f2fbf5e7926f06663a407388dd63e7b98762c
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-09-03 14:10:23 +0900

    done 1-13

--- progate/sql1/exercise.sql
+++ progate/sql1/exercise.sql
@@ -105,6 +105,29 @@ FROM purchases
 WHERE category = "食費"
   OR character_name = "にんじゃわんこ";
 
+-- 「FROM purchases」のあとにコードを追加し、priceカラムを基準に降順に並び替えたデータを取得してください
 
+SELECT *
+FROM purchases
+ORDER BY price DESC;
 
+-- 「WHERE character_name = "にんじゃわんこ"」のあとにコードを追加し、priceカラムを基準に昇順に並び替えたデータを取得してください
 
+SELECT *
+FROM purchases
+WHERE character_name = "にんじゃわんこ"
+ORDER BY price ASC;
+
+-- 「FROM purchases」のあとにコードを追加し、取得結果が「最大5件」となるようにデータを取得してください。
+
+SELECT *
+FROM purchases
+LIMIT
+5;
+
+-- 「WHERE character_name = "にんじゃわんこ"」のあとにコードを追加し、取得結果が「最大10件」となるようにデータを取得してください。
+
+SELECT *
+FROM purchases
+WHERE character_name = "にんじゃわんこ"
+LIMIT 10;
