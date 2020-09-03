5d2599655a1c67c582992c9814c344c47f46f616
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-09-04 00:56:05 +0900

    done 3-3

--- progate/sql3/exercise.sql
+++ progate/sql3/exercise.sql
@@ -22,3 +22,13 @@ WHERE goals > (
   SELECT AVG(goals)
 FROM players
 );
+
+--ASを使って、取得したカラム名を"身長180cm以上の選手"としてください
+SELECT name AS "身長180cm以上の選手"
+FROM players
+WHERE height >= 180;
+
+--ASを使って、取得したカラム名を"チームの合計得点"としてください
+SELECT SUM(goals) AS "チームの合計得点"
+FROM players;
+
