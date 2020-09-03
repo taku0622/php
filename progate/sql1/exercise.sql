3fb2b1732436bebcfbae38f9de2ae64760f89edc
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-09-03 13:21:57 +0900

    done 1-9

--- progate/sql1/exercise.sql
+++ progate/sql1/exercise.sql
@@ -65,3 +65,21 @@ WHERE name LIKE "プリン%";
 SELECT *
 FROM purchases
 WHERE name LIKE "%プリン";
+
+-- 「FROM purchases」のあとにコードを追加し、character_nameカラムが「にんじゃわんこ」でないデータを取得してください
+
+SELECT *
+FROM purchases
+WHERE NOT character_name = "にんじゃわんこ";
+
+-- 「FROM purchases」のあとにコードを追加し、nameカラムが「プリン」を含まないデータを取得してください
+
+SELECT *
+FROM purchases
+WHERE NOT name LIKE "%プリン%";
+
+
+
+
+
+
