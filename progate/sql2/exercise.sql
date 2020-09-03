84eb4156bd155844e96010eec21a78b4c0bbcd5c
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-09-03 23:57:08 +0900

    done 2-10

--- progate/sql2/exercise.sql
+++ progate/sql2/exercise.sql
@@ -109,3 +109,19 @@ FROM purchases
 WHERE category = "食費"
 GROUP BY purchased_at,character_name
 ;
+
+-- 日付ごとの合計金額のうち、2000円を超えるデータのみを取得してください
+
+SELECT SUM(price), purchased_at
+FROM purchases
+GROUP BY purchased_at
+HAVING SUM(price) > 2000
+;
+
+-- 日付とキャラクターごとの合計金額のうち、3000円を超えるデータのみを取得してください
+
+SELECT SUM(price), purchased_at, character_name
+FROM purchases
+GROUP BY purchased_at,character_name
+HAVING SUM(price) > 3000
+;
