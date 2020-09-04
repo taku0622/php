d5e807c29d42a8c69e4267ee607ca291dafb7ac1
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-09-04 19:47:00 +0900

    done dojo-6

--- progate/sql3/dojo.sql
+++ progate/sql3/dojo.sql
@@ -61,3 +61,15 @@ WHERE price <= 7000
   FROM items
   WHERE name = "グレーパーカー"
 );
+
+-- 商品ごとに商品のid、売れた個数を取得してください
+SELECT item_id, COUNT(*)
+FROM sales_records
+GROUP BY item_id;
+
+-- 売れた数が多い上位5商品のidと個数を取得してください
+SELECT item_id, COUNT(*)
+FROM sales_records
+GROUP BY item_id
+ORDER BY COUNT(*) DESC
+LIMIT 5;
