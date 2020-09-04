f20fae06f8978e18f554aa580dd8975687336721
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-09-04 19:24:26 +0900

    done dojo-3

--- progate/sql3/dojo.sql
+++ progate/sql3/dojo.sql
@@ -12,3 +12,17 @@ WHERE age < 20
 SELECT age, COUNT(*)
 FROM users
 GROUP BY age;
+
+-- 全商品の名前を重複無く取得してください
+SELECT DISTINCT(name)
+FROM items;
+
+-- 全商品の名前と値段を、値段が高い順に並べてください
+SELECT name, price
+FROM items
+ORDER BY price DESC;
+
+-- 名前の一部に「シャツ」を含む商品の、全てのカラムの値を取得してください
+SELECT *
+FROM items
+WHERE name LIKE "%シャツ%";
