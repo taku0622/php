b35a2f15021af7ce085a8bbd785886fee36a9426
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-09-04 19:39:07 +0900

    done dojo-5

--- progate/sql3/dojo.sql
+++ progate/sql3/dojo.sql
@@ -40,3 +40,24 @@ SELECT name, price-cost
 FROM items
 ORDER BY price-cost DESC
 LIMIT 5;
+
+-- 「グレーパーカー」より値段が高い商品の名前と値段を取得してください
+SELECT name
+, price
+FROM items
+WHERE price >
+(
+  SELECT price
+FROM items
+WHERE name = "グレーパーカー"
+);
+
+-- 7000円以下で「グレーパーカー」より利益が高い商品を取得してください
+SELECT name, price - cost
+FROM items
+WHERE price <= 7000
+  AND (price - cost) > (
+  SELECT price - cost
+  FROM items
+  WHERE name = "グレーパーカー"
+);
