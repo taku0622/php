674f30c7e6796bc00152074d3da0181889448263
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-09-04 21:21:03 +0900

    done dojo-10

--- progate/sql3/dojo.sql
+++ progate/sql3/dojo.sql
@@ -123,3 +123,37 @@ FROM sales_records
   ON sales_records.item_id = items.id
 WHERE items.name = "サンダル"
 GROUP BY sales_records.user_id
+
+-- 男性向け、女性向け、男女兼用商品ごとに指定されたデータを取得してください
+SELECT items.gender, SUM(items.price) AS "売上額"
+FROM items
+  JOIN sales_records
+  ON items.id = sales_records.item_id
+GROUP BY items.gender;
+
+-- 売上額が上位5位の商品の指定されたデータを取得してください
+SELECT items.id, items.name, SUM(items.price) AS "売上額"
+FROM items
+  JOIN sales_records
+  ON items.id = sales_records.item_id
+GROUP BY items.id
+ORDER BY SUM(items.price) DESC
+LIMIT 5;
+
+-- グレーパーカーより売上額が高い商品の指定されたデータを取得してください
+SELECT items
+.id, items.name, SUM
+(items.price) AS "売上額"
+FROM items
+  JOIN sales_records
+  ON items.id = sales_records.item_id
+GROUP BY items.id
+HAVING SUM
+(items.price) >
+(
+  SELECT SUM(items.price)
+FROM items
+  JOIN sales_records
+  ON items.id = sales_records.item_id
+WHERE items.name = "グレーパーカー"
+)
