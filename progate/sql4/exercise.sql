1a073f1ce95c41162a826d13913d2893a64374b6
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-09-04 22:29:45 +0900

    done 4-2

--- progate/sql4/exercise.sql
+++ progate/sql4/exercise.sql
@@ -5,3 +5,12 @@ VALUES( "Kate", "Java");
 -- 下記のクエリは消さないでください。
 select *
 from students;
+
+-- studentsテーブルのidカラムの値が6のレコードをnameをJordanに、courseをHTMLに変更してください。
+UPDATE students
+SET name = "Jordan", course = "HTML"
+WHERE id = 6;
+-- 下記のクエリは消さないでください。
+SELECT *
+FROM students
+WHERE id=6;
