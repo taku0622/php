8b7afeb583363d0409f9def5073ed538b6dab197
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-09-05 14:20:46 +0900

    done 4-12

--- PHP_book/chapter4.sql
+++ PHP_book/chapter4.sql
@@ -32,3 +32,22 @@ WHERE id=1;
 SELECT *
 FROM my_items
 WHERE id=1;
+
+-- c12
+UPDATE my_items SET item_name="いちご",price=180
+WHERE id=1;
+
+INSERT INTO my_items
+SET item_name
+="りんご", price=90;
+
+INSERT INTO my_items
+SET item_name
+="バナナ", price=120;
+
+INSERT INTO my_items
+SET item_name
+="ブルーベリー", price=220;
+
+UPDATE my_items SET price=200
+WHERE item_name="ブルーベリー";
